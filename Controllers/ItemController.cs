ea7129a78bfdd50cf0ce2cddadf1e39a276a581b
jocunda <silviawanshan88@gmail.com>
2023-09-11 20:25:33 +0800

    Add Item Controller and Model

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RicherBE.Models;
+
+namespace RicherBE.Controllers
+  {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ItemController : ControllerBase
+    {
+    [HttpGet]
+    [Route("item") ]
+    public async Task<ActionResult> GetAllItems ()
+      {
+      return await _itemService.GetAllItems();
+      }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetSingleItem (int id)
+      {
+      var result = await _itemService.GetSingleItem(id);
+      if(result is null)
+        return NotFound("Item Not Found")
+      return Ok(result);
+      }
+
+    [HttpPost]
+    public async Task<ActionResult> AddItem ()
+      {
+      var result = await _itemService.AddItem();
+      return Ok(result);
+      }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteItem(int id)
+      {
+      var result = await _itemService.DeleteItem(id);
+      if ( result is null )
+        return NotFound("Item not found");
+      return Ok(result);
+      }
+
+
+    }
+  }
---
+++ Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace RicherBE.Models
+  {
+  public class Item
+    {
+    public string Id { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Photo { get; set; } = string.Empty;
+    public bool? Deleteable { get; set; }
+    public string CreatedById { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }= DateTime.Now;
+    }
+  }
